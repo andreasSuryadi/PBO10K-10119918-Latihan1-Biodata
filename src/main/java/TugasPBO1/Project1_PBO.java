f2405c82e520ed39f15d038a217ec1b6e168a715
Andreas Suryadi <andreas.suryadi02@gmail.com>
2020-10-20 19:32:02 +0700

    PBO6-10119918-Latihan1-Biodata

---
+++ src/main/java/TugasPBO1/Project1_PBO.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package TugasPBO1;
+
+/**
+ *
+ * @author andreas
+ */
+public class Project1_PBO {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("BIODATA ROCKSTAR" + "\n" + "--------------------------");
+        System.out.println("NAMA\t\t: Andreas Suryadi");
+        System.out.println("ALAMAT\t\t: Jalan Astana Anyar no. 292");
+        System.out.println("TEMPAT LAHIR\t: Bandung");
+        System.out.println("TANGGAL LAHIR\t: 27 Juli 2001");
+        System.out.println("--------------------------");
+        System.out.println("HOBI\t\t: Coding");
+        System.out.println("MAKES\t\t: Sate");
+        System.out.println("MIKES\t\t: Air Mineral");
+    }
+    
+}
